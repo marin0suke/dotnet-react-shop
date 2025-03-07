137ef1b5fd19a19eede5ed66e9f30981c8555847
marin0suke <monk.marina@gmail.com>
2025-03-07 15:26:03 +1000

    Add RegisterForm component

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -3,31 +3,27 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
-const LoginForm: React.FC = () => { 
+const RegisterForm: React.FC = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState<string | null>(null);
-
     const navigate = useNavigate();
 
     const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault(); 
-
+        e.preventDefault();
         try {
-            const response = await axios.post("http://localhost:5000/api/auth/login", { email, password }); // 
-            const token = response.data.token;
-            localStorage.setItem("token", token); // stores token under the key "token".
-            navigate("/products");
+            await axios.post("http://localhost:5000/api/auth/register", { email, password });
+            navigate("/login");
         } catch (err: unknown) {
-            setError("Login failed - please check credentials");
+            setError("Registration failed. Please enter valid email and password");
         }
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <h2>Login</h2>
-                {error && <p style={{color: "red"}}>{error}</p>} 
+                <h2>Register</h2>
+                {error && <p style={{ color: "red" }}>{error}</p>}
                 <input 
                     type="email"
                     placeholder="Email"
@@ -42,13 +38,10 @@ const LoginForm: React.FC = () => {
                     onChange={e => setPassword(e.target.value)}
                     required
                 />
-                <button type="submit">Login</button>
+                <button type="submit">Register</button>
             </form>
         </div>
     )
-}
+};
 
-export default LoginForm;
-
-// track email password, takes form input from client to send to service layer for auth. 
-// handleSubmit from form. 
+export default RegisterForm;
